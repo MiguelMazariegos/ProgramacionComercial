5df8d21136ef05c7a495656017289256af4f731c
Miguel Mazariegos <miguelmazariegos1997@gmail.com>
2019-09-09 12:43:00 -0600

    Se agregaron los formularios de crear y editar un post nuevo Se agrego validacion para la autenticacion del usuario, para crear o editar un post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.listar_pub, name='listar_pub'),
     path('pub/<int:pk>/', views.detalle_pub, name='detalle_pub'),
+    path('pub/new', views.post_new, name='nueva_pub'),
+    path('post/<int:pk>/edit/', views.post_edit, name='editar_pub'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
 from django.shortcuts import render, get_object_or_404
+from .forms import PublicacionForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def listar_pub(request):
@@ -12,3 +14,32 @@ def listar_pub(request):
 def detalle_pub (request, pk):
     pub = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/detalle_pub.html', { 'pub': pub })
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PublicacionForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detalle_pub', pk=post.pk)
+    else:
+        form = PublicacionForm()
+    return render(request, 'blog/pub_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PublicacionForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detalle_pub', pk=post.pk)
+    else:
+        form = PublicacionForm(instance=post)
+    return render(request, 'blog/pub_edit.html', {'form': form})
