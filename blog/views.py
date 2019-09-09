9ff228f77289f2ff6ad1f58b1246a273a276b051
Miguel Mazariegos <miguelmazariegos1997@gmail.com>
2019-09-09 11:59:12 -0600

    Agregadas las vistas para las todas las publicaciones y para una sola publicacion Se agrego el estilo en bootstrap de las paginas

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Publicacion
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def listar_pub(request):
     pubs = Publicacion.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/listar_pub.html', {'pubs': pubs})
+
+
+def detalle_pub (request, pk):
+    pub = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_pub.html', { 'pub': pub })
